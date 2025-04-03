16ca92926a6209dc89c1bb567e835a9f3a168830
gregorychernyavskiy <gregory.chernyavskiy@gmail.com>
2025-04-03 11:57:37 -0500

    Solved.

---
+++ #3 - 242. Valid Anagram/242.valid-anagram.cs
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode id=242 lang=csharp
+ *
+ * [242] Valid Anagram
+ */
+
+// @lc code=start
+public class Solution {
+    public bool IsAnagram(string s, string t) {
+        Dictionary<char, int> map = new Dictionary<char, int>();
+
+        foreach(char sch in s) {
+            if(map.ContainsKey(sch)) {
+                map[sch]++;
+            } else {
+                map[sch] = 1;
+            }
+        }
+        foreach(char tch in t) {
+            if(map.ContainsKey(tch)) {
+                map[tch]--;
+                if(map[tch] < 1) {
+                    return false;
+                }
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+// @lc code=end
+
