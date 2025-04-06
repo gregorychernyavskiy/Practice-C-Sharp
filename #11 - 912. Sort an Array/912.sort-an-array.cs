a15b9feeec7cb02b24636323bbbcff96f01f7692
gregorychernyavskiy <gregory.chernyavskiy@gmail.com>
2025-04-06 14:42:34 -0500

    Solved using min/max.

--- #11 - 912. Sort an Array/912.sort-an-array.cs
+++ #11 - 912. Sort an Array/912.sort-an-array.cs
@@ -11,7 +11,7 @@ public class Solution {
     }
 
     private int[] MergeSort(int[] arr, int L, int R) {
-        if(L == R) {
+        if(L >= R) {
             return arr;
         }
         int M = (L + R) / 2;
--- #9 - 705. Design HashSet/705.design-hash-set.cs
+++ #9 - 705. Design HashSet/705.design-hash-set.cs
@@ -6,21 +6,21 @@
 
 // @lc code=start
 public class MyHashSet {
-
+    BitArray arr;
     public MyHashSet() {
-        
+        arr = new BitArray(1000001);
     }
     
     public void Add(int key) {
-        
+        arr[key] = true;
     }
     
     public void Remove(int key) {
-        
+        arr[key] = false;
     }
     
     public bool Contains(int key) {
-        
+        return arr[key];
     }
 }
 
