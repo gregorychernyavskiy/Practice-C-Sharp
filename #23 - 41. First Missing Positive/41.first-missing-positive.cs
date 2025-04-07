3ab9f70c25f9af7a204809ea528427ad2d469bc2
gregorychernyavskiy <gregory.chernyavskiy@gmail.com>
2025-04-07 14:52:19 -0500

    First hard

---
+++ #23 - 41. First Missing Positive/41.first-missing-positive.cs
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode id=41 lang=csharp
+ *
+ * [41] First Missing Positive
+ */
+
+// @lc code=start
+public class Solution {
+    public int FirstMissingPositive(int[] nums) {
+        
+    }
+}
+// @lc code=end
+
