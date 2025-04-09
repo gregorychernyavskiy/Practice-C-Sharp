82b7c785f5f99b1506099b4a47fc5d9b23aaf856
gregorychernyavskiy <gregory.chernyavskiy@gmail.com>
2025-04-09 09:09:25 -0500

    2 solutions, one with stringbuilder and second one with 3 for loops, both answers in O(n).

---
+++ #27 - 1768. Merge Strings Alternately/1768.merge-strings-alternately.cs
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode id=1768 lang=csharp
+ *
+ * [1768] Merge Strings Alternately
+ */
+
+// @lc code=start
+public class Solution {
+    public string MergeAlternately(string word1, string word2) {
+        
+    }
+}
+// @lc code=end
+
