9688370a0eae1449b8574672ae48bd543b43d372
gregorychernyavskiy <gregory.chernyavskiy@gmail.com>
2025-04-09 09:54:20 -0500

    2 solutions, one with stringbuilder and second one with 3 for loops, both answers in O(n).

--- #27 - 1768. Merge Strings Alternately/1768.merge-strings-alternately.cs
+++ #27 - 1768. Merge Strings Alternately/1768.merge-strings-alternately.cs
@@ -7,8 +7,43 @@
 // @lc code=start
 public class Solution {
     public string MergeAlternately(string word1, string word2) {
-        
+        StringBuilder st = new StringBuilder();
+        int max = Math.Max(word1.Length, word2.Length);
+
+        for(int i = 0; i < max; i++) {
+            if(i < word1.Length) {
+                st.Append(word1[i]);
+            }
+            if(i < word2.Length) {
+                st.Append(word2[i]);
+            }
+        }
+        return st.ToString();
     }
 }
+
+// public class Solution {
+//     public string MergeAlternately(string word1, string word2) {
+//         int len1 = word1.Length;
+//         int len2 = word2.Length;
+//         int minLength = Math.Min(len1, len2);
+//         char[] arr = new char[len1 + len2];
+//         int j = 0;
+        
+//         for (int i = 0; i < minLength; i++) {
+//             arr[j++] = word1[i];
+//             arr[j++] = word2[i];
+//         }
+
+//         for (int i = minLength; i < len1; i++) {
+//             arr[j++] = word1[i];
+//         }
+        
+//         for (int i = minLength; i < len2; i++) {
+//             arr[j++] = word2[i];
+//         }
+//         return new string(arr);
+//     }
+// }
 // @lc code=end
 
