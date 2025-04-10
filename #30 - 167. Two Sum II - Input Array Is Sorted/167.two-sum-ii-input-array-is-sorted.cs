2004d1cb991c04e87db6add41852f83c45e84fe8
gregorychernyavskiy <gregory.chernyavskiy@gmail.com>
2025-04-10 10:20:14 -0500

    Two pointers for 2Sum.

---
+++ #30 - 167. Two Sum II - Input Array Is Sorted/167.two-sum-ii-input-array-is-sorted.cs
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode id=167 lang=csharp
+ *
+ * [167] Two Sum II - Input Array Is Sorted
+ */
+
+// @lc code=start
+public class Solution {
+    public int[] TwoSum(int[] numbers, int target) {
+        int left = 0;
+        int right = numbers.Length - 1;
+
+        while (left < right) {
+            int sum = numbers[left] + numbers[right];
+
+            if (sum == target) {
+                return new int[] { left + 1, right + 1 };
+            } else if (sum > target) {
+                right--;
+            } else {
+                left++;
+            }
+        }
+        return new int[] {-1, -1};
+    }
+}
+
+// @lc code=end
+
