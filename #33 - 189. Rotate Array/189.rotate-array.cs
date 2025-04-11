95a4b51a985aa64cacf2be2bc57486c6346ba2b6
gregorychernyavskiy <gregory.chernyavskiy@gmail.com>
2025-04-11 10:35:47 -0500

    Two solutions, one with swap which is faster, and the seconf one with 3 for loops.

--- #33 - 189. Rotate Array/189.rotate-array.cs
+++ #33 - 189. Rotate Array/189.rotate-array.cs
@@ -24,5 +24,30 @@ public class Solution {
         }
     }
 }
+
+// public class Solution {
+//     public void Rotate(int[] nums, int k) {
+//         int[] arr = new int[nums.Length];
+//         int count = 0;
+
+//         if(k == 0 || k == null) return;
+//         if(nums.Length == 0 || nums.Length == null) return;
+
+//         k = k % nums.Length;
+//         if(k == 0) return;
+
+//         for(int i = nums.Length - k; i < nums.Length; i++) {
+//             arr[count] = nums[i];
+//             count++;
+//         }
+//         for(int i = 0; i < nums.Length - k; i++) {
+//             arr[count] = nums[i];
+//             count++;
+//         }
+//         for(int i = 0; i < nums.Length; i++) {
+//             nums[i] = arr[i];
+//         }
+//     }
+// }
 // @lc code=end
 
