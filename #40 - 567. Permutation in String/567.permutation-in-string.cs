e70157c47b25baa269191fdac4581a9713bf8abe
gregorychernyavskiy <gregory.chernyavskiy@gmail.com>
2025-04-14 11:23:35 -0500

    Done.

---
+++ #40 - 567. Permutation in String/567.permutation-in-string.cs
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode id=567 lang=csharp
+ *
+ * [567] Permutation in String
+ */
+
+// @lc code=start
+public class Solution {
+    public bool CheckInclusion(string s1, string s2) {
+        
+    }
+}
+// @lc code=end
+
