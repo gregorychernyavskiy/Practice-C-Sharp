675a6ef442f3540639f05b439afcf2af79f55b19
gregorychernyavskiy <gregory.chernyavskiy@gmail.com>
2025-04-16 16:27:16 -0500

    Done.

---
+++ #46 - 225. Implement Stack using Queues/225.implement-stack-using-queues.cs
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode id=225 lang=csharp
+ *
+ * [225] Implement Stack using Queues
+ */
+
+// @lc code=start
+public class MyStack {
+
+    public MyStack() {
+        
+    }
+    
+    public void Push(int x) {
+        
+    }
+    
+    public int Pop() {
+        
+    }
+    
+    public int Top() {
+        
+    }
+    
+    public bool Empty() {
+        
+    }
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.Push(x);
+ * int param_2 = obj.Pop();
+ * int param_3 = obj.Top();
+ * bool param_4 = obj.Empty();
+ */
+// @lc code=end
+
