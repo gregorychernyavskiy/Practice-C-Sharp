8346e63c1c960b574baa0b8263b5afb846285eb3
gregorychernyavskiy <gregory.chernyavskiy@gmail.com>
2025-04-26 17:28:17 -0500

    .

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,5 @@
+﻿using ConsoleApp1.scr.OOP.Encapsulation;
+
+BadBankAccounts BadAccount = new BadBankAccounts();
+BadAccount.balance = 100;
+System.Console.WriteLine(BadAccount.balance);
---
+++ ConsoleApp1/scr/OOP/Encapsulation/BadBankAccounts.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.scr.OOP.Encapsulation
+{
+    public class BadBankAccounts
+    {
+        public decimal balance;
+    }
+}
